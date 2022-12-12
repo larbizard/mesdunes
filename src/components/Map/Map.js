8da473a24c4d8302241dc500c99133cf85f5acb1
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2022-12-12 17:13:25 -0500

    Fixed geojson displayed when location refused

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -35,7 +35,7 @@ const DEFAULT_CENTER = [33.36081, -8.29610]
 
 const ROAD_MARKER = [33.352025922821454, -8.295728560922754]
 
-const { MapContainer, Marker, Popup } = ReactLeaflet;
+const { MapContainer, Marker, Popup, GeoJSON } = ReactLeaflet;
 
 const Map = ({ children, className, ...rest }) => {
   let mapClassName = styles.map;
@@ -68,10 +68,75 @@ const Map = ({ children, className, ...rest }) => {
       </Marker>
       <Marker icon={TurnLeftIcon} position={ROAD_MARKER}>
         <Popup>
-          Mes dunes.
+          Route côtière
         </Popup>
       </Marker>
-      
+      <GeoJSON data={{
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "properties": {},
+                    "geometry": {
+                        "coordinates": [
+                            [
+                                -8.295658308045859,
+                                33.352025925727744
+                            ],
+                            [
+                                -8.294867949785385,
+                                33.35258342022529
+                            ],
+                            [
+                                -8.2950962755055,
+                                33.35275947037755
+                            ],
+                            [
+                                -8.295517799911465,
+                                33.354563963908234
+                            ],
+                            [
+                                -8.296395975756269,
+                                33.355986993399256
+                            ],
+                            [
+                                -8.29662430147502,
+                                33.3569112165863
+                            ],
+                            [
+                                -8.2969053177452,
+                                33.35742466967136
+                            ],
+                            [
+                                -8.29620277707042,
+                                33.357938119727905
+                            ],
+                            [
+                                -8.294780132201055,
+                                33.35880364296595
+                            ],
+                            [
+                                -8.295904197283107,
+                                33.35994788081429
+                            ],
+                            [
+                                -8.295921760798905,
+                                33.360153255245905
+                            ],
+                            [
+                                -8.296413539272038,
+                                33.360740036665135
+                            ],
+                            [
+                                -8.29611495948609,
+                                33.3609454092273
+                            ]
+                        ],
+                        "type": "LineString"
+                    }
+                }
+            ]
+        }} />
     </MapContainer>
   )
 }
